3f90ed416f5badfd145780205d845cbe70ca4816
Grego Cabrera <gregorioct@gmail.com>
2021-05-29 11:42:59 +0200

    Fix url, eliminado de www

--- client/src/utils/Url.js
+++ client/src/utils/Url.js
@@ -1,4 +1,4 @@
-const url = "https://www.speedcyclo.herokuapp.com/"
+const url = "https://speedcyclo.herokuapp.com/"
 //const url = "http://localhost:5000/"
 export const getUrl = () => {
     return url
